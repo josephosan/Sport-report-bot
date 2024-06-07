db1ff73b9e652df5dae5ac4d5fecded073dd8e56
joseph.o <josephosan1381@gmail.com>
2024-06-07 16:59:24 +0330

    waiting to inser user logs into db

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
-const express = require('express')
 const { Pool } = require('pg')
 const { Telegraf } = require('telegraf')
-
+const schedule = require('node-schedule')
+const winston = require('winston')
 
 // ==================== base constants ==================== //
 const api_token = '7225296585:AAEhMD18-ORk0At8_POKvStNTzOPFCr9xdk'
@@ -23,31 +23,42 @@ const api = axios.create({
     baseURL
 })
 const bot = new Telegraf(api_token)
-const app = express()
-app.use(express.json())
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'application.log' })
+    ]
+});
 // ==================== app and api ==================== //
 
 
 
 // ==================== base variables ==================== //
 let users
-const PORT = 3000
+const privilegedUsernames = ['josephosan']
+const acceptedKeywords = ['DONE', 'COMPLETE', 'I\'VE DONE MY WORKOUT', 'TODAY IS DONE']
+const repeatMessageTime = '0 17 * * *'
 // ==================== base variables ==================== //
 
 
 // ==================== database functions ==================== //
 const insertUser = async (user) => {
     let sameUser = false
-    const getAllQuery = 'SELECT * FROM users'
+    const findUser = `SELECT * FROM users WHERE username = '${user.username}'`
     try {
-        const { rows } = await pool.query(getAllQuery)
-        sameUser = rows.some(u => u.username === user.username)
+        const { rows } = await pool.query(findUser)
+        sameUser = rows.length
     } catch (err) {
-        console.log(err)
+        console.log(err.message)
     }
 
     if (sameUser) {
-        console.log('This user already exists on db!')
+        logger.warn('User exists', { message: 'This user already exists on db!' })
         return
     }
 
@@ -59,18 +70,74 @@ const insertUser = async (user) => {
 
     try {
         const { rows } = await pool.query(query, [user.username, user.chat_id, user.is_bot, user.language_code])
-        console.log(rows)
     } catch (err) {
-        console.log(err)
+        logger.error('Catch Error', err.message)
     }
+}
+
+const updateUsersDailyState = async (username) => {
+    let userData = undefined
+    const getUserQuery = `SELECT * FROM users WHERE username = '${username}'`
+
+    try {
+        const { rows } = await pool.query(getUserQuery)
+        userData = rows[0]
+    } catch (err) {
+        console.log(err.message)
+    }
+
+    if (!userData) {
+        logger.warn('USER NOT FOUND', { message: "requested user not found!" })
+        return
+    }
+
+    console.log(userData)
 }
 // ==================== database functions ==================== //
 
 // ==================== tel bot functions ==================== //
-bot.start((ctx) => ctx.reply('Hi!'))
-bot.help((ctx) => ctx.reply('You asked help, None of my business!'))
-bot.on('message', (ctx) => {
-    console.log(ctx)
+bot.start(async (ctx) => {
+    await getUpdates()
+    logger.info('Bot start', { username: ctx.update.message.from.username })
+    ctx.reply('Hi! Welcome.')
+})
+bot.help((ctx) => {
+    const replyText = `
+    🌟 *Daily Workout Bot Features* 🌟
+
+    1. **Completion Detection** 🏆
+    - When the bot receives a message containing any of the following phrases: "DONE", "COMPLETE", "I'VE DONE MY WORKOUT", or "TODAY IS DONE", it will mark your daily workout as completed.
+
+    2. **Daily Reminder** ⏰
+    - The bot will send you a friendly reminder every day at 17:00 to keep you on track with your fitness goals.
+
+    3. **Coach Privileges** 🏋️‍♂️
+    - If you are a coach (a user with full access to the bot), you can use the following keywords for additional functionalities:
+        - **"GLOBAL MESSAGE:"** 🌐: Use this keyword in your message to notify all members.
+        - **"GET REPORT"** 📊: Use this keyword to view users' daily statuses.
+    `;
+
+    // Send the formatted reply
+    ctx.replyWithMarkdownV2(escapeMarkdown(replyText));
+})
+bot.on('message', async (ctx) => {
+    // handling privileged users
+    if (privilegedUsernames.includes(ctx.update.message.from.username)) {
+        if (ctx.update.message.text.includes('GLOBAL MESSAGE:')) {
+            await messageAllUsers(ctx.update.message.text.split('GLOBAL MESSAGE:')[1])
+            return
+        }
+    }
+
+    // handle workout done
+    if (acceptedKeywords.includes(ctx.update.message.text)) {
+        await updateUsersDailyState(ctx.update.message.from.username)
+        ctx.reply('🏋️‍♂️ GOOD JOB. YOUR CHANGES ARE SAVED!')
+        return
+    }
+
+    // if nothing, reply
+    ctx.reply(`THIS IS NO COMMAND!`)
 })
 
 
@@ -87,12 +154,12 @@ const getUpdates = async () => {
         }).filter(item => item)
 
         const insertPromises = users.map(user => {
-            return insertUser(user).catch(err => console.log(err))
+            return insertUser(user).catch(err => logger.error('Catch Error', err.message))
         })
 
         return Promise.all(insertPromises)
     } catch (err) {
-        console.log(err)
+        logger.error('Catch Error', err.message)
     }
 }
 
@@ -103,11 +170,11 @@ const messageAllUsers = async (message = 'Global') => {
         const { rows } = await pool.query(getAllQuery)
         if (rows) dbUsers = rows
     } catch (err) {
-        console.log(err)
+        logger.error('Catch Error', err.message)
     }
 
     if (!dbUsers || !dbUsers.length) {
-        console.log("NO USER IN DB!")
+        logger.warn('No user in db!', { message: 'There is no user in database to notify!' })
         return
     }
 
@@ -121,25 +188,30 @@ const messageAllUsers = async (message = 'Global') => {
 const getMe = async () => {
     try {
         const { data } = await api.get('getMe')
-        console.log(data)
     } catch (err) {
-        console.log(err)
+        logger.error('Catch Error', err.message)
     }
-}
-
-const updateAndSendMessage = async () => {
-    await getUpdates()
-    await messageAllUsers()
-    console.log('done!')
 }
 // ==================== tel bot functions ==================== //
 
 
+// ==================== running jobs ==================== //
+const globalMessageJob = schedule.scheduleJob(repeatMessageTime, async () => {
+    await messageAllUsers('Did you do your workout?')
+})
+// ==================== running jobs ==================== //
+
+// ==================== utils ==================== //
+const escapeMarkdown = (text) => {
+    return text.replace(/([_*[\]()~`>#+\-=|{}.!])/g, '\\$1');
+};
+// ==================== utils ==================== //
+
+
+
+
 bot.launch().then(() => {
-    console.log('Bot launched successfully');
+    logger.info('Bot Launched!')
 }).catch(err => {
-    console.error('Error launching bot:', err.message);
+    logger.error('Catch Error', err.message)
 });
-// app.listen(PORT, () => {
-//     console.log(`LISTENING ON PORT ${PORT} ...`)
-// })

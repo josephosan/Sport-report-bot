093a40f716787d0846a40c006ecd994c80ef8bf0
joseph.o <josephosan1381@gmail.com>
2024-06-15 15:02:38 +0330

    moved previlaged usernames to env file

--- config/config.ts
+++ config/config.ts
@@ -3,7 +3,7 @@ require('dotenv').config()
 export const api_token = process.env.API_TOKEN
 export const baseURL = `https://api.telegram.org/bot${api_token}/`
 
-export const privilegedUsernames = ['josephosan', 'duniyalr']
+export const privilegedUsernames = process.env.PRIVILEGED_USERNAMES ? process.env.PRIVILEGED_USERNAMES.split(',') : []; 
 export const acceptedKeywords = ['DONE', 'COMPLETE', 'I\'VE DONE MY WORKOUT', 'TODAY IS DONE']
 export const reportKeyWord = 'REPORT:'
 export const globalMessageKeyWord = 'GLOBAL MESSAGE:'

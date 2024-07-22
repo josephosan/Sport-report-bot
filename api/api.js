ce7f258d64e31ce3a44b5a063dd221b7089bda46
Joseph <joseph@Josephs-MacBook-Air.local>
2024-07-22 17:51:34 +0330

    removed js

--- api/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.api = void 0;
-var axios_1 = require("axios");
-var config_1 = require("../config/config");
-exports.api = axios_1.default.create({
-    baseURL: config_1.baseURL
-});

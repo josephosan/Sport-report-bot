b4e27bbea119fb170090e5b4c7b2308ab3d2c7f1
Joseph <joseph@Josephs-MacBook-Air.local>
2024-11-07 08:17:06 +0330

    some fix

--- index.ts
+++ index.ts
@@ -16,15 +16,6 @@ schedule.scheduleJob(dailyInitTime, async () => {
 schedule.scheduleJob(every5Hours, async () => {
     await getCurrencies()
 })
-schedule.scheduleJob('10 14 * * *', async () => {
-    await messageAllUsers('متین متین متین ...')
-})
-schedule.scheduleJob('0 22 * * *', async () => {
-    await messageAllUsers('ماهیتابه')
-})
-schedule.scheduleJob('30 23 * * *', async () => {
-    await messageAllUsers('سامان مسواک بزن')
-})
 
 bot.launch().then(() => {
     logger.info('Bot Launched!')

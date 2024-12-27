20e7940b352ad8232d5582a2b6509ddc9ebac719
Joseph <joseph@Josephs-MacBook-Air.local>
2024-12-27 15:24:28 +0330

    fix

--- AI/index.ts
+++ AI/index.ts
@@ -14,7 +14,7 @@ export const basicPrompt = async (
   const prompt = `${ai_base_prompt} \n ${userName} says: ${p} \n and prev messages are: ${prevMessages}`;
   try {
     const result = await model.generateContent(prompt);
-    return result;
+    return result.response.text();
   } catch (err) {
     logger.error('Catch on ai basic prompt', { message: err });
   }
